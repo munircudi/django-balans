9412a026f38c5ae1f56803e1d1d3ee82465b359d
munircudi <38553707+munircudi@users.noreply.github.com>
2023-08-17 21:23:08 +0300

    Update settings.py

--- Balans/balans/balans/settings.py
+++ Balans/balans/balans/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+from os import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,11 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#   BASE_DIR / "static"
+# ]
 
-STATIC_ROOT = BASE_DIR / "static_files"
+
+ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
